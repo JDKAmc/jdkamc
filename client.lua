5febe5c6328290b09a8b76a8ffaaa0966c7374e4
itsJota29 <61953611+itsJota29@users.noreply.github.com>
2020-12-25 19:34:59 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -3,7 +3,7 @@ local msg			= ''
 
 Citizen.CreateThread(function()
 	while ESX == nil do
-		TriggerEvent('esx:getShGeGenesisRParedObjGeGenesisRPect', function(obj) ESX = obj end)
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
     end
 end)
